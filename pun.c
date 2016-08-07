5b87c297789553dfff5a91d4b4fdb8d8ac0e5ed1
Greg Geiger <greg.e.geiger@gmail.com>
2016-08-07 16:47:02 -0500

    ADDED: Added comment lines at beginning of program.

--- pun.c
+++ pun.c
@@ -1,3 +1,14 @@
+//
+// 2016-08-07:geg
+//
+// Example program on page 10.
+//
+// Text: "C Programming : A Modern Approach"
+// Edition: 2nd (August 2008)
+// Author: K. N. King
+//
+// Copyright (c) 2016 G Geiger
+//
 #include <stdio.h>
 
 int main (void)
