636f9c5ae72ef696f7d45116a52d7ee74cff995c
Greg Geiger <greg.e.geiger@gmail.com>
2016-08-14 07:35:29 -0500

    Initial version.

---
+++ ch2_ex4.c
@@ -0,0 +1,30 @@
+//
+// 2016-08-14:geg
+//
+// Exercise program 4 on page 33.
+//
+// Text: "C Programming : A Modern Approach"
+// Edition: 2nd (August 2008)
+// Author: K. N. King
+//
+// Copyright (c) 2016 G Geiger
+//
+// Create several int and float vars without initializing them
+// Print these
+//
+ 
+
+#include <stdio.h>
+
+int main (void)
+{
+  int var1,var2,var3;
+  float fvar1,fvar2,fvar3;
+  
+  
+  printf("Int variables: %d %d %d\n",var1,var2,var3);
+
+  printf("Float variables: %f %f %f\n",fvar1,fvar2,fvar3);
+
+  return (0);
+}
