63705fb24c18c6a738352f99eaabdb0f059869d5
Greg Geiger <greg.e.geiger@gmail.com>
2016-08-20 07:54:37 -0500

    Initial version.

---
+++ ch2_proj1.c
@@ -0,0 +1,36 @@
+//
+// 2016-08-14:geg
+//
+// Programming project 1 on page 34.
+//
+// Text: "C Programming : A Modern Approach"
+// Edition: 2nd (August 2008)
+// Author: K. N. King
+//
+// Copyright (c) 2016 G Geiger
+//
+// Use printf to print ascii art check mark
+//
+//        * 8 spaces on left
+//       *
+//      *
+// *   *
+//  * *
+//   *
+//
+
+#include <stdio.h>
+
+
+int main (void)
+{
+
+  printf ("        *\n");
+  printf ("       *\n");
+  printf ("      *\n");
+  printf ("*    *\n");
+  printf (" *  *\n");
+  printf ("  *\n");
+
+  return (0);
+}
