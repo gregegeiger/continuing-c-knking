a0ee2e28e2520cecfdd626cf49d582e03ccf33f7
Greg Geiger <greg.e.geiger@gmail.com>
2016-08-27 06:35:17 -0500

    Initial version.

---
+++ Chapter_3/ch3_proj4.c
@@ -0,0 +1,39 @@
+//
+// 2016-08-27:geg
+//
+// Programming project 4 on page 50.
+//
+// Text: "C Programming : A Modern Approach"
+// Edition: 2nd (August 2008)
+// Author: K. N. King
+//
+// Copyright (c) 2016 G Geiger
+//
+// Prompt user for phone number and print this number with a different
+// format.
+//    
+//
+// http://knking.com/books/c2/answers/index.html
+//
+#include <stdio.h>
+
+int main (void)
+{
+  int area_code,exchange,subscriber;
+
+  //
+  // The North American Numbering Plan NANP:
+  // NPA: numbering plan area code: Allowed ranges: [2–9] for the first digit
+  //                                and [0-9] for the second and third digits.
+  // Central office (exchange) code: exchange code: Allowed ranges: [2–9] for
+  //                                the first digit and [0-9] for the
+  //                                second and third digits.
+  // Subscriber number (station code) : Allowed ranges: [0-9] for four digits
+  //
+  printf ("Enter phone number [(xxx) xxx-xxxx]: ");
+  scanf ("(%d)%d-%d",&area_code,&exchange,&subscriber);
+
+  printf ("You entered: %3d.%3d.%4d\n",area_code,exchange,subscriber);
+  
+  return (0);
+}
