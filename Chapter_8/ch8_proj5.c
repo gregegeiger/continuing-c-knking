942a16ac6db0000ece95b57dd31034bf7c6ab58a
Greg Geiger <greg.e.geiger@gmail.com>
2016-11-12 16:09:37 -0600

    Initial version.

---
+++ Chapter_8/ch8_proj5.c
@@ -0,0 +1,68 @@
+//
+// 2016-11-12:geg
+//
+// Programming project 5 on page 178.
+//
+// Text: "C Programming : A Modern Approach"
+// Edition: 2nd (August 2008)
+// Author: K. N. King
+//
+// Copyright (c) 2016 G Geiger
+//
+// Modify the program interest.c on page 169 to compound the interest monthly and still display the
+// value yearly
+//
+//
+// http://knking.com/books/c2/answers/index.html
+//
+#include <stdbool.h>
+#include <stdio.h>
+//
+// 
+#define NUM_RATES ((int) (sizeof(value)/sizeof(value[0])))
+//
+// Initial investment
+//
+#define INITIAL_BALANCE 100.00
+
+
+int main (void)
+{
+  int i, low_rate, num_years, year;
+  double value[5];
+
+  printf("Enter interest rate: ");
+  scanf("%d",&low_rate);
+
+  printf("Enter number of years: ");
+  scanf("%d",&num_years);
+  //
+  // column header for the years column
+  printf("\nYears");
+  //
+  // column headers for the interest rate columns
+  for (i=0; i<NUM_RATES; i++){
+    printf("%6d%%",low_rate + i);
+    //
+    // Set all interest columns to their initial balance
+    value[i]=INITIAL_BALANCE;
+    //    printf("    i: %d bal: %6.2f    \n",i,value[i]);
+  }
+  printf("\n");
+
+  //
+  // Print the year and the values for that year across all rate columns
+  for (year=1; year<=num_years; year++) {
+    //
+    // Print the year
+    printf("%3d    ",year);
+    for (i = 0; i < NUM_RATES; i++) {
+      value[i] += (low_rate + i)/100.0 * value[i];
+      printf("%7.2f", value[i]);
+    }
+    printf("\n");
+  }
+
+  
+  return (0);
+}
