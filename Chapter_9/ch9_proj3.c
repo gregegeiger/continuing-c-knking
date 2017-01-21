a9ad03a872e32954f8687c8048a8058dc86a1bae
Greg Geiger <greg.e.geiger@gmail.com>
2017-01-21 07:44:19 -0600

    Initial version.

---
+++ Chapter_9/ch9_proj3.c
@@ -0,0 +1,163 @@
+//
+// 2017-01-21:geg
+//
+// Programming project 3 on page 217.
+//
+// Text: "C Programming : A Modern Approach"
+// Edition: 2nd (August 2008)
+// Author: K. N. King
+//
+// Copyright (c) 2017 G Geiger
+//
+// Generate a random walk on a 10 x 10 array
+//
+//
+// http://knking.com/books/c2/answers/index.html
+//
+#include <stdbool.h>
+#include <stdio.h>
+
+//
+//
+//
+void print_array(int n,int map[n][n]);
+
+
+int main (void)
+{
+  //
+  // Create the map or 10 x 10 grid for the walk and initialize it to '.'
+  //
+  int map[10][10] = { {'.','.','.','.','.','.','.','.','.','.'},
+		      {'.','.','.','.','.','.','.','.','.','.'},
+		      {'.','.','.','.','.','.','.','.','.','.'},
+		      {'.','.','.','.','.','.','.','.','.','.'},
+		      {'.','.','.','.','.','.','.','.','.','.'},
+		      {'.','.','.','.','.','.','.','.','.','.'},
+		      {'.','.','.','.','.','.','.','.','.','.'},
+		      {'.','.','.','.','.','.','.','.','.','.'},
+		      {'.','.','.','.','.','.','.','.','.','.'},
+		      {'.','.','.','.','.','.','.','.','.','.'} };
+		      
+  //
+  // Start in the upper left of the map
+  //
+  int x=0,
+    y=0,
+    index=0,
+    move=0,
+    valid=0;
+  //
+  // Start with character @
+  //
+  int character = 64;
+  //
+  // Maximum tries
+  //
+  int max_move_count = 126;
+  int move_count = 0;
+
+  //
+  // Map print variables
+  //
+  int map_x = 0;
+  int map_y = 0;
+
+
+  //
+  // Initialize random number generator
+  //
+  srand(time(NULL));
+
+  for (index=0; index < max_move_count; index++) {
+    //
+    // Reset for next move
+    //
+    valid=0;
+    while (valid == 0) {
+      //
+      // Increment move counter
+      //
+      if (move_count++ > max_move_count) { break; }
+      //
+      // Calculate random move value 0 through 3
+      //
+      move = rand() % 4;
+
+      // printf("move:%i\n",move);
+
+      switch(move) {
+	//
+	// move=0 : up
+	//
+	
+      case 0: 
+	//	printf("case 0 move %i\n",move);
+	if (y+1>=0 && y+1 <=9 && map[x][y+1] == '.') {
+	  y=y+1;
+	  valid=1;
+	  map[x][y] = character++;
+	}
+	break;
+	//
+	// move=1 : right
+	//
+      case 1: 
+	// printf("case 1 move %i\n",move);
+	if (x+1>=0 && x+1<=9 && map[x+1][y] == '.') {
+	  x=x+1;
+	  valid=1;
+	  map[x][y] = character++;
+	}
+	break;
+	//
+	// move=2 : down
+	//
+      case 2: 
+	// printf("case 2 move %i\n",move);
+	if (y-1>=0 && y-1<=9 && map[x][y-1] == '.') {
+	  y=y-1;
+	  valid=1;
+	  map[x][y] = character++;
+	};
+	break;
+	//
+	// move=3 : left
+	//
+      case 3: 
+	// printf("case 3 move %i\n",move);
+	if (x-1>=0 && x-1<=9 && map[x-1][y] == '.') {
+	  x=x-1;
+	  valid=1;
+	  map[x][y] = character++;
+	};
+	break;
+      }
+    }
+
+  }
+
+  print_array(10,map);
+
+      
+
+
+  return (0);
+
+}
+
+
+void print_array( int map_size, int map[map_size][map_size]) {
+
+  int map_x = 0;
+  int map_y = 0;
+  
+  // Print map
+  for (map_y=0; map_y < map_size; map_y++) {
+    for (map_x=0; map_x < map_size; map_x++) {
+      printf("%c ",map[map_x][map_y]);
+    }
+    printf("\n");
+  }
+  return;
+}  
