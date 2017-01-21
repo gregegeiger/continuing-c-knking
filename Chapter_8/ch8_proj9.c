7d6c694f83f0fef2a21a029a407e4a9debf30ece
Greg Geiger <greg.e.geiger@gmail.com>
2017-01-21 07:19:26 -0600

    CHANGED: Implement map

--- Chapter_8/ch8_proj9.c
+++ Chapter_8/ch8_proj9.c
@@ -16,26 +16,22 @@
 //
 #include <stdbool.h>
 #include <stdio.h>
-//
-// 
-#define NUM_STUDENTS 5
-#define NUM_TESTS 5
 
 int main (void)
 {
   //
-  // Create the map or 10 x 10 grid for the walk and initialize it to 0
+  // Create the map or 10 x 10 grid for the walk and initialize it to '.'
   //
-  int map[10][10] = { {0,0,0,0,0,0,0,0,0,0},
-		      {0,0,0,0,0,0,0,0,0,0},
-		      {0,0,0,0,0,0,0,0,0,0},
-		      {0,0,0,0,0,0,0,0,0,0},
-		      {0,0,0,0,0,0,0,0,0,0},
-		      {0,0,0,0,0,0,0,0,0,0},
-		      {0,0,0,0,0,0,0,0,0,0},
-		      {0,0,0,0,0,0,0,0,0,0},
-		      {0,0,0,0,0,0,0,0,0,0},
-		      {0,0,0,0,0,0,0,0,0,0} };
+  int map[10][10] = { {'.','.','.','.','.','.','.','.','.','.'},
+		      {'.','.','.','.','.','.','.','.','.','.'},
+		      {'.','.','.','.','.','.','.','.','.','.'},
+		      {'.','.','.','.','.','.','.','.','.','.'},
+		      {'.','.','.','.','.','.','.','.','.','.'},
+		      {'.','.','.','.','.','.','.','.','.','.'},
+		      {'.','.','.','.','.','.','.','.','.','.'},
+		      {'.','.','.','.','.','.','.','.','.','.'},
+		      {'.','.','.','.','.','.','.','.','.','.'},
+		      {'.','.','.','.','.','.','.','.','.','.'} };
 		      
   //
   // Start in the upper left of the map
@@ -45,19 +41,49 @@ int main (void)
     index=0,
     move=0,
     valid=0;
+  //
+  // Start with character @
+  //
+  int character = 64;
+  //
+  // Maximum tries
+  //
+  int max_move_count = 126;
+  int move_count = 0;
 
-  
+  //
+  // Map print variables
+  //
+  int map_x = 0;
+  int map_y = 0;
+
+  // Print empty map
+
+//   for (map_y = 0; map_y < 10; map_y++) {
+// 
+//     for (map_x=0; map_x < 10; map_x++) {
+// 
+//       printf("%c ",map[map_x][map_y]);
+// 
+//     }
+//     printf("\n");
+//   }
+//   
   //
   // Call the random number function
   // rand() returns a number between 0 and RAND_MAX
   //
   // We want a random result 0 through 3
   //
-  for (index=0; index<20; index++) {
+  for (index=0; index < max_move_count; index++) {
     //
     // Reset for next move
     //
     valid=0;
+    //
+    // Increment move counter
+    //
+    if (move_count++ > max_move_count) { return 0; }
     
     while (valid == 0) {
       //
@@ -65,54 +91,72 @@ int main (void)
       //
       move = rand() % 4;
 
-      printf("move:%i\n",move);
+      // printf("move:%i\n",move);
 
       switch(move) {
 	//
 	// move=0 : up
 	//
+	
       case 0: 
-	printf("case 0 move %i\n",move);
-	if (y+1>=0 && y+1 <=9) {
+	//	printf("case 0 move %i\n",move);
+	if (y+1>=0 && y+1 <=9 && map[x][y+1] == '.') {
 	  y=y+1;
 	  valid=1;
+	  map[x][y] = character++;
 	}
 	break;
 	//
 	// move=1 : right
 	//
       case 1: 
-	printf("case 1 move %i\n",move);
-	if (x+1>=0 && x+1<=9) {
+	// printf("case 1 move %i\n",move);
+	if (x+1>=0 && x+1<=9 && map[x+1][y] == '.') {
 	  x=x+1;
 	  valid=1;
+	  map[x][y] = character++;
 	}
 	break;
 	//
 	// move=2 : down
 	//
       case 2: 
-	printf("case 2 move %i\n",move);
-	if (y-1>=0 && y-1<=9) {
+	// printf("case 2 move %i\n",move);
+	if (y-1>=0 && y-1<=9 && map[x][y-1] == '.') {
 	  y=y-1;
 	  valid=1;
+	  map[x][y] = character++;
 	};
 	break;
 	//
 	// move=3 : left
 	//
       case 3: 
-	printf("case 3 move %i\n",move);
-	if (x-1>=0 && x-1<=9) {
+	// printf("case 3 move %i\n",move);
+	if (x-1>=0 && x-1<=9 && map[x-1][y] == '.') {
 	  x=x-1;
 	  valid=1;
+	  map[x][y] = character++;
 	};
 	break;
       }
     }
 
-    printf("X:%i Y:%i Index:%i\n",x,y,index);
+    //    printf("X:%i Y:%i Index:%i\n",x,y,index);
+    //
+
+      
   }
+
+
+    // Print map
+    for (map_y=0; map_y < 10; map_y++) {
+      for (map_x=0; map_x < 10; map_x++) {
+	printf("%c ",map[map_x][map_y]);
+      }
+      printf("\n");
+    }
+
   return (0);
 
 }
