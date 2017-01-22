cfe98534141c7568cf899c01df00036919bbb785
Greg Geiger <greg.e.geiger@gmail.com>
2017-01-22 11:47:48 -0600

    FIXED: Replaced recursion call with simple flag. CHANGED: Change map to 21 x 21

--- Chapter_9/ch9_proj3.c
+++ Chapter_9/ch9_proj3.c
@@ -17,8 +17,9 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <unistd.h> //needed for sleep function
+#include <math.h>
 
-#define MAP_SIZE 20
+#define MAP_SIZE 21
 //
 // Function to print the map
 //
@@ -37,9 +38,6 @@ int main (void)
   //
   int map[MAP_SIZE][MAP_SIZE];
 
-  generate_random_walk(MAP_SIZE,map);
-
-		      
   //
   // Start in the middle of the map
   //
@@ -48,20 +46,11 @@ int main (void)
     index=0,
     move=0,
     valid=0;
+
   //
   // Place the character A at the starting point
   //
   int character = 65;
-
-
-  while(1){
-  generate_random_walk(MAP_SIZE,map);
-
-
-  map[x][y] = character++;
-
-  print_array(MAP_SIZE,map);
-
 
 
   //
@@ -69,7 +58,9 @@ int main (void)
   //
   int max_move_count = 500;
   int move_count = 0;
-
+  //
+  // Trapped flag
+  int trapped = 0;
   //
   // Map print variables
   //
@@ -77,112 +68,142 @@ int main (void)
   int map_y = 0;
 
 
-  //
-  // Initialize random number generator
-  //
-  srand(time(NULL));
+  
+  while(1){
+    //
+    // Initialize the map
+    //
+    
+    generate_random_walk(MAP_SIZE,map);
 
-  for (index=0; index < max_move_count; index++) {
+    //
+    // Start in middle of map
+    //
+    x=floor(MAP_SIZE/2);
+    y=floor(MAP_SIZE/2);
+    character=65;
+
+    move=0;
+    move_count=0;
+
+
+    map[x][y] = character++;
 
     print_array(MAP_SIZE,map);
 
-    if (character != 'A') {
-      sleep(1);
-    }
+
 
     //
-    // Reset for next move
+    // Initialize random number generator
     //
-    valid=0;
-    while (valid == 0) {
+    srand(time(NULL));
 
-      //
-      // Increment move counter
-      //
-      if (move_count++ > max_move_count) {
-	printf("\n\n\n\nTrapped!\n\n\n\n");
-        sleep(5);
-	system("/home/greg/git/continuing-c-knking/Chapter_9/a.out");
+    for (index=0; index < max_move_count; index++) {
+
+      print_array(MAP_SIZE,map);
+
+      if (character != 'A') {
+	sleep(1);
       }
-      //
-      // Calculate random move value 0 through 3
-      //
-      move = rand() % 4;
 
-      // printf("move:%i\n",move);
+      //
+      // Reset for next move
+      //
+      valid=0;
+      trapped = 0;
+      while (valid == 0 && trapped == 0) {
 
-      switch(move) {
 	//
-	// move=0 : up
+	// Increment move counter
 	//
+	if (move_count++ > max_move_count) {
+	  printf("\n\n\n\nTrapped!\n\n\n\n");
+	  sleep(5);
+	  //
+	  // Set flags to drop out of this map
+	  //
+	  trapped = 1;
+	  index = max_move_count;
+	}
+	//
+	// Calculate random move value 0 through 3
+	//
+	move = rand() % 4;
+
+	// printf("move:%i\n",move);
+
+	switch(move) {
+	  //
+	  // move=0 : up
+	  //
 	
-      case 0: 
-	//	printf("case 0 move %i\n",move);
-	if (y+1>=0 && y+1 < MAP_SIZE && map[x][y+1] == '.') {
-	  y=y+1;
-	  valid=1;
-	  map[x][y] = character++;
+	case 0: 
+	  //	printf("case 0 move %i\n",move);
+	  if (y+1>=0 && y+1 < MAP_SIZE && map[x][y+1] == '.') {
+	    y=y+1;
+	    valid=1;
+	    map[x][y] = character++;
+	  }
+	  break;
+	  //
+	  // move=1 : right
+	  //
+	case 1: 
+	  // printf("case 1 move %i\n",move);
+	  if (x+1>=0 && x+1< MAP_SIZE && map[x+1][y] == '.') {
+	    x=x+1;
+	    valid=1;
+	    map[x][y] = character++;
+	  }
+	  break;
+	  //
+	  // move=2 : down
+	  //
+	case 2: 
+	  // printf("case 2 move %i\n",move);
+	  if (y-1>=0 && y-1< MAP_SIZE && map[x][y-1] == '.') {
+	    y=y-1;
+	    valid=1;
+	    map[x][y] = character++;
+	  };
+	  break;
+	  //
+	  // move=3 : left
+	  //
+	case 3: 
+	  // printf("case 3 move %i\n",move);
+	  if (x-1>=0 && x-1< MAP_SIZE && map[x-1][y] == '.') {
+	    x=x-1;
+	    valid=1;
+	    map[x][y] = character++;
+	  };
+	  break;
 	}
-	break;
+
 	//
-	// move=1 : right
+	// If the next character is '[' change to 'a'
+	// We don't want [ \ ] ^ _ and ' on the map
 	//
-      case 1: 
-	// printf("case 1 move %i\n",move);
-	if (x+1>=0 && x+1< MAP_SIZE && map[x+1][y] == '.') {
-	  x=x+1;
-	  valid=1;
-	  map[x][y] = character++;
-	}
-	break;
+	if (character == '[') {character = 'a';}
+
 	//
-	// move=2 : down
+	// If the next character is '{' change to '0'
+	// We don't want { | } ~ on the map
 	//
-      case 2: 
-	// printf("case 2 move %i\n",move);
-	if (y-1>=0 && y-1< MAP_SIZE && map[x][y-1] == '.') {
-	  y=y-1;
-	  valid=1;
-	  map[x][y] = character++;
-	};
-	break;
+	if (character == '{') {character = '0';}
+
 	//
-	// move=3 : left
+	// If the next character is ':' change to '@'
 	//
-      case 3: 
-	// printf("case 3 move %i\n",move);
-	if (x-1>=0 && x-1< MAP_SIZE && map[x-1][y] == '.') {
-	  x=x-1;
-	  valid=1;
-	  map[x][y] = character++;
-	};
-	break;
+	if (character == ':') {character = '@';}
+
+
       }
 
-      //
-      // If the next character is '[' change to 'a'
-      // We don't want [ \ ] ^ _ and ' on the map
-      //
-      if (character == '[') {character = 'a';}
-
-      //
-      // If the next character is '{' change to '0'
-      // We don't want { | } ~ on the map
-      //
-      if (character == '{') {character = '0';}
-
-      //
-      // If the next character is ':' change to '@'
-      //
-      if (character == ':') {character = '@';}
-
-
     }
-
-  }
 
       
-  printf ("\n\n\n\n\n\n\n\n");
+    printf ("\n\n\n\n\n\n\n\n");
   }
   return (0);
 
