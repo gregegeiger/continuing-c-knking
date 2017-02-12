30cfea4d934336b4928ec2920ef9c4a1d0729dde
Greg Geiger <greg.e.geiger@gmail.com>
2017-02-12 08:06:56 -0600

    CHANGED: Add comments to split function.

--- Chapter_9/ch9_qsort.c
+++ Chapter_9/ch9_qsort.c
@@ -85,29 +85,56 @@ void quicksort(int a[], int low, int high) {
 //*****************************************************************************
 // Split function
 //
+// Select the element at index low to be the partitioning element part_element.  Arrange all
+// elements less than part_element to the left of the part_element in the array and
+// all elements greater than part_element to the right of the part_element in the array.
+//
 
 int split(int a[], int low, int high) {
+  //
+  // Move the low element, selected as the partitioning element, to a temp variable
+  //
   int part_element = a[low];
 
   for (;;) {
+    //
+    // Walk the array from the high element down until we find an element less than part_element
+    //
     while (low < high && part_element <= a[high]) {
       high--;
     }
-
+    //
+    // When low equals high we are done.
+    //
     if (low >= high) break;
-
+    //
+    // Swap the high element and the low element.  Move the low index up one in the array.
+    //
     a[low++] = a[high];
-
+    //
+    // Walk the array from the low element up until we find an element greater than part_element
+    //
     while(low < high && a[low] <= part_element) {
       low++;
     }
-
+    //
+    // When low equals high we are done.
+    //
     if (low >= high) break;
-
+    //
+    // Swap the high element and the low element.  Move the high index down one in the array.
+    //
     a[high--] = a[low];
+    //
+    // Repeat.
+    //
   }
-
+  //
+  // Replace the partition element into the high position.
+  //
   a[high] = part_element;
-
+  //
+  // Return the high index
+  //
   return(high);
 }
